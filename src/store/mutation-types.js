c57f7d887509d5bdfa4b7f01868b20e846094b7d
Jeff Wen <hi@sinchang.me>
2019-02-26 12:06:47 +0000

    Deploy sinchang/doba to github.com/sinchang/doba.git:gh-pages

--- src/store/mutation-types.js
+++
@@ -1,7 +0,0 @@
-export const FETCH_INTHEATERS = 'FETCH_INTHEATERS'
-export const FETCH_SUBJECT = 'FETCH_SUBJECT'
-export const FETCH_CELEBRITY = 'FETCH_CELEBRITY'
-export const SHOW_LOADING = 'SHOW_LOADING'
-export const HIDE_LOADING = 'HIDE_LOADING'
-export const SEARCH = 'SEARCH'
-export const GET_KEYWORD = 'GET_KEYWORD'
